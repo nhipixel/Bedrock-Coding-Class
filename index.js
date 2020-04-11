659cbf2e4d9efc65203f5c077ef8cfb233673568
nhipixel <nhiingo146@gmail.com>
2020-04-11 11:49:29 +0700

    session2-turtlejs

---
+++ index.js
@@ -0,0 +1,41 @@
+//hinh 1
+clear();
+color ("red");
+for (let i=0; i<4; i++) {
+lt(30);
+fd(100);
+rt(60);
+fd(100);
+rt(120);
+fd(100);
+rt(60);
+fd(100);
+rt(60);
+}
+
+//hinh 2
+clear();
+color ("red");
+for (let i=0; i<4; i++) {
+fd(100);
+rt(90);
+}
+lt(30);
+
+for (let i=0; i<5; i++) {
+fd(100);
+rt(60);
+}
+
+color ("blue");
+
+rt(60);
+fd(100);
+lt(120)
+fd(100);
+rt(60);
+
+for (let i=0; i<4; i++) {
+rt(72);
+fd(100);
+}
