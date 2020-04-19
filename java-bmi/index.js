d4aca6c72a98f8cfc725af9093b48e45200fc561
nhipixel <63487093+nhipixel@users.noreply.github.com>
2020-04-19 18:07:35 +0700

    Update index.js

--- java-bmi/index.js
+++ java-bmi/index.js
@@ -1,19 +1,21 @@
-//BMI program
+//BMI program chua bai
 
-let height = Number (prompt("Please enter your height (cm)")) * Number(0.01);
-let mass = Number (prompt("Please enter your weight (kg)"));
+var height = Number(prompt("Please enter your height (cm)"));
+var mass = Number(prompt("Please enter your weight (kg)"));
 
-BMI = mass/(height*height);
+height = height/100;
+
+var BMI = mass/(height*height);
 
 console.log("Your BMI is " + BMI);
 
 if (BMI<16) {
     alert("You are severely underweight.");
-} else if (16<=BMI<=18.5) {
+} else if (16<= BMI && BMI <=18.5) {
     alert("You are underweight.");
-} else if (18.5<BMI<25) {
+} else if (18.5<BMI && BMI <25) {
     alert("You are normal.");
-} else if (25<=BMI<=30) {
+} else if (25<=BMI && BMI <=30) {
     alert("You are overweight.");
 } else if (BMI>30) {
     alert("You are obese.");
